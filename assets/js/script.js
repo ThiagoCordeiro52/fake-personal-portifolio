b10a14971e9af2d17b802a58b9d6cab08c371571
Thiago Cordeiro <thiagoty52@gmail.com>
2024-04-19 16:39:58 -0300

    feat: create testimonials area

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,28 @@ document.querySelector(".menu-opener").addEventListener("click", () => {
   const button = document.querySelector("header .menu-opener");
   button.classList.toggle("open");
 });
+
+function toggleSlider() {
+  const slides = document.querySelectorAll("input[name=slider]");
+  if (slides.length === 0) return;
+
+  let current = document.querySelector("input[name=slider]:checked");
+  if (!current) {
+    current = document.querySelector("input[name=slider]");
+    current.setAttribute("checked", true);
+  }
+
+  let id = Number(current.getAttribute("id").split("-")[1]);
+  if (id + 1 <= slides.length) {
+    id++;
+  } else {
+    id = 1;
+  }
+
+  for (let slider of slides) {
+    slider.removeAttribute("checked");
+  }
+  document.querySelector(`#slider-${id}`).setAttribute("checked", true);
+}
+
+setInterval(toggleSlider, 5000);
